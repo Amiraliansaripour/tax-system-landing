faef509c8ba8027beb47586245195d88b686b04b
amiraliansaripour <amiraliansaripor1383@gmail.com>
2024-08-05 10:07:30 +0330

    page flip images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,64 +1,62 @@
-import bigShoe1 from './screen1.png'
+import bigShoe1 from "./screen1.png";
 // import bigShoe2 from './big-shoe2.png'
 // import bigShoe3 from './big-shoe3.png'
 
-import footerLogo from './footer-logo.svg'
-import headerLogo from './logo.png'
+import footerLogo from "./footer-logo.svg";
+import headerLogo from "./logo.png";
 
-import offer from './offer.svg'
+import offer from "./offer.svg";
 
-import Enamad from './enamadpng.png'
+import Enamad from "./enamadpng.png";
 
 // import shoe4 from './shoe4.svg'
 // import shoe5 from './shoe5.svg'
 // import shoe6 from './shoe6.svg'
 // import shoe7 from './shoe7.svg'
-import shoe8 from './screen2.png'
+import shoe8 from "./screen2.png";
 
-import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailBackground from "./thumbnail-background.svg";
 // import thumbnailShoe1 from './thumbnail-shoe1.svg'
 // import thumbnailShoe2 from './thumbnail-shoe2.svg'
 // import thumbnailShoe3 from './thumbnail-shoe3.svg'
 
-import Screen1 from './screen1.png'
+import Screen1 from "./screen1.png";
 
+import Sscreen from "./1.png";
+import AppScreen from "./2.png";
+import AppScreen2 from "./3.png";
+import AppScreen3 from "./4.png";
 
-import Sscreen from './1.png'
-import AppScreen from './2.png'
-import AppScreen2 from './3.png'
-import AppScreen3 from './4.png'
+import Slide1 from "./slide-1.png";
+import Slide2 from "./slide-2.png";
 
-import Slide1 from './slide-1.png'
-import Slide2 from './slide-2.png'
-
+// book pages
+import page1 from "./book-page/1.png";
+import page2 from "./book-page/2.png";
+import page3 from "./book-page/3.png";
+import page4 from "./book-page/4.png";
+import page5 from "./book-page/5.png"
 export {
-    bigShoe1,
-    // bigShoe2,
-    // bigShoe3,
-    footerLogo,
-    headerLogo,
+  bigShoe1,
 
-    offer,
-    // shoe4,
-    // shoe5,
-    // shoe6,
-    // shoe7,
-    shoe8,
-
-    thumbnailBackground,
-    // thumbnailShoe1,
-    // thumbnailShoe2,
-    // thumbnailShoe3,
-
-    Screen1,
-
-    Sscreen,
-    Enamad,
-
-    Slide1,
-    Slide2,
-
-    AppScreen,
-    AppScreen2,
-    AppScreen3
-}
+  footerLogo,
+  headerLogo,
+  offer,
+  shoe8,
+  thumbnailBackground,
+ 
+  Screen1,
+  Sscreen,
+  Enamad,
+  Slide1,
+  Slide2,
+  AppScreen,
+  AppScreen2,
+  AppScreen3,
+  
+  page1,
+  page2,
+  page3,
+  page4,
+  page5
+};
