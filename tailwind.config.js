6e6b9c89c7b92dffe9e22cd22834da2b7a49684b
amiraliansaripour <amiraliansaripor1383@gmail.com>
2024-08-05 10:06:28 +0330

    installed react-pageflip

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ export default {
         "box":"#222831",
         "slate-gray": "#6D6D6D",
         "pale-blue": "#F5F6FF",
-        "white-400": "rgba(255, 255, 255, 0.80)"
+        "white-400": "rgba(255, 255, 255, 0.80)",
       },
       boxShadow: {
         '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
